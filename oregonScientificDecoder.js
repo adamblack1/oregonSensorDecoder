87599d55adc4ce760fb49b4c83d101daef08807d
Adam Black <adam>
2013-07-22 14:55:24 +1000

    wrote a failing test that the last word of data converted is correct

--- oregonScientificDecoder.js
+++ oregonScientificDecoder.js
@@ -1,3 +1,13 @@
-function getResult() {
-	return true;
+
+//function createReading // http://www.w3schools.com/js/js_obj_intro.asp
+
+function getOregonWords(input) {
+	var result = [];
+
+	//TODO: fix the order...!!!!!!!!
+	for (var i = 0; i < 16; i++) {
+		result[i] = 0;
+	}
+	return result;
 }
+
--- test.js
+++ test.js
@@ -4,6 +4,13 @@ test( "hello test", function() {
   ok( 1 == "1", "Passed!" );
 });
 
-test ("get result", function() {
-	ok(getResult());
+test ("Test getOregonWords - length", function() {
+	var data = getOregonWords(0x12345678);
+	equal(data.length, 16);
 });
+
+test ("Test getOregonWords - data at end", function() {
+	var data = getOregonWords(0xFFFFFFF1);
+	equal(data[15], 1);
+});
+
