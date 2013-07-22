a25dc2cc9d99126ad2cf5f1515a3d278cb52cfc3
Adam Black <adam>
2013-07-22 16:29:45 +1000

    working on adding get temperature

--- oregonScientificDecoder.js
+++ oregonScientificDecoder.js
@@ -1,13 +1,40 @@
 
 //function createReading // http://www.w3schools.com/js/js_obj_intro.asp
 
-function getOregonWords(input) {
-	var result = [];
+var WORD_SIZE_IN_BITS = 8;
+var MESSAGE_LENGTH_IN_WORDS = 16;
 
-	//TODO: fix the order...!!!!!!!!
-	for (var i = 0; i < 16; i++) {
-		result[i] = 0;
-	}
+
+function OregonReading(rawData) {
+	var reading = new Object();
+
+	var dataAsWords = getOregonWords(rawData);
+	reading.temperature = getOregonTemperature(dataAsWords);
+
+	return reading;
+}
+
+//TODO: add support for handling negative temperatures
+function getOregonTemperature(dataAsWords) {
+	var tens = dataAsWords[10];
+	var ones = dataAsWords[11];
+	var tenths = dataAsWords[13];
+
+	alert("tens are "+tens);
+	return tens * 10 + ones + tenths / 10;
+}
+
+
+function getOregonWords(input) {
+	var wordsAsAString = input.toString(WORD_SIZE_IN_BITS);
+	var result = wordsAsAString.split("");
+	padArrayToExpectedLength(result);
+
 	return result;
 }
 
+function padArrayToExpectedLength(array) {
+	while (array.length < MESSAGE_LENGTH_IN_WORDS) {
+		array.push(0);
+	}
+}
--- test.js
+++ test.js
@@ -1,16 +1,21 @@
-
-
 test( "hello test", function() {
   ok( 1 == "1", "Passed!" );
 });
 
-test ("Test getOregonWords - length", function() {
-	var data = getOregonWords(0x12345678);
-	equal(data.length, 16);
-});
-
-test ("Test getOregonWords - data at end", function() {
+test("Test getOregonWords - data at end", function() {
 	var data = getOregonWords(0xFFFFFFF1);
 	equal(data[15], 1);
 });
 
+test("Test getOregonWords - length", function() {
+	var data = getOregonWords(0x12345678);
+	equal(data.length, 16);
+});
+
+//TODO: test padding e.g. data / that other things have not shifted
+
+
+test("Test get temperature", function(assert) {
+	var reading = OregonReading(0xEA4C209C51274074);
+	assert.close(reading.temperature, 27.5, 0.01);
+});
