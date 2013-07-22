cc28c64bc3d7e06d31634883a5b04c6fe55ee22a
Adam Black <adam>
2013-07-22 14:19:17 +1000

    added oregonscientific decoder and ran an empty test using it

---
+++ oregonScientificDecoder.js
@@ -0,0 +1,3 @@
+function getResult() {
+	return true;
+}
--- test.js
+++ test.js
@@ -1,3 +1,9 @@
+
+
 test( "hello test", function() {
   ok( 1 == "1", "Passed!" );
+});
+
+test ("get result", function() {
+	ok(getResult());
 });
